d81f25addb7b4f16180738f92a10cb5933178660
Carolina Lucca <carolinalucca@Carolinas-MacBook-Pro.local>
2020-01-10 20:53:40 -0300

    list of books TDD

---
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/ListBooksTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ListBooksTest {
+
+    @Test
+    public void returnListOfBooks() {
+        assertEquals(new ArrayList<String>(), new ArrayList<String>());
+    }
+}
