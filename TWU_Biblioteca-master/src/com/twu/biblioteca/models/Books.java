63794178ca832f122a9ee357c5d23fe9e76afa2b
Carolina Lucca <carolinalucca@Carolinas-MacBook-Pro.local>
2020-01-11 14:03:22 -0300

    return book title test

---
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/models/Books.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca.models;
+
+public class Books {
+
+    private String title;
+
+    public Books(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
--- TWU_Biblioteca-master/test/com/twu/biblioteca/itemList/BookListTest.java
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/itemList/BookListTest.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca.itemList;
 
+import com.twu.biblioteca.models.Books;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,8 +10,15 @@ import static org.junit.Assert.assertEquals;
 
 public class BookListTest {
 
+    private Books book;
+
+    @Before
+    public void init() {
+        book = new Books("Mr. Mercedes");
+    }
+
     @Test
-    public void returnListOfBooks() {
-        assertEquals(new ArrayList<String>(), new ArrayList<String>());
+    public void getBookTitle() {
+        assertEquals("Mr. Mercedes", book.getTitle());
     }
 }
